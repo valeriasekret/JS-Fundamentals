4bf6e59d63ecbaaad66c69ce3ee002d9c9afef84
valeriasekret <leraguerra@gmail.com>
2023-03-27 16:26:28 +0200

    added topic 6 homework

--- JSF-topic-6/shoe-task/shoes.js
+++ JSF-topic-6/shoe-task/shoes.js
@@ -1,43 +1,71 @@
-/*6. Задачthis.ink -= this.decrementFn();а описана в блоці JS
+/*6. Задача описана в блоці JS
 Реалізувати логіку підрахунку ціни товару по його варіаціях(шаблон наведений тут, при кліку на колір змінювати ціну товару).
 Придумати ще 2 варіації, відяких буде залежати ціна товару.
  */
 
 const colorButtonsContainer = document.querySelector(".colors");
+const sizeButtonsContainer = document.querySelector(".sizes");
+const deliveryOptionsContainer = document.querySelector(".options");
 const price = document.getElementById("outprice");
-colorButtonsContainer.addEventListener("click", (event) => changeColor(event));
+let activeColor = document.querySelector(".colors .active");
+let activeSize = null;
+let deliveryOption = null;
+
+colorButtonsContainer.addEventListener("click", (event) => {
+  changeColor(event);
+  changePic();
+});
+sizeButtonsContainer.addEventListener("click", (event) => changeSize(event));
+deliveryOptionsContainer.addEventListener("click", (event) => changeDelivery(event));
+
 
 function changeColor(event) {
   if (event.target.classList.contains("color")) {
-    const activeColor = document.querySelector(".colors .active");
     activeColor.classList.remove("active");
     event.target.classList.add("active");
+    activeColor = event.target;
     const newPrice = event.target.getAttribute("data-price");
     price.innerText = newPrice;
-    const newColor = event.target.getAttribute("color");
-    const shoeImage = document.querySelector(`.shoe[color="${newColor}"]`);
-    const shoeImageSrc = shoeImage.getAttribute("src");
-    const newShoeImageSrc = shoeImageSrc.replace(/\/\w+\.png$/, `/${newColor}.png`);
-    shoeImage.setAttribute("src", newShoeImageSrc);
   }
 }
 
+function changePic() {
+  const images = document.querySelectorAll(".shoe");
+  images.forEach((shoe) => {
+    if (shoe.getAttribute("color") === activeColor.getAttribute("color")) {
+      shoe.classList.add("show");
+    } else {
+      shoe.classList.remove("show");
+    }
+  });
+}
 
+function changeSize(event) {
+  if (event.target.classList.contains("size")) {
+    const activeSizeButton = document.querySelector(".sizes .active");
+    if (activeSizeButton) {
+      activeSizeButton.classList.remove("active");
+    }
+    event.target.classList.add("active");
+    activeSize = event.target.getAttribute("data-price");
+    updatePrice();
+  } 
+}
 
+function changeDelivery(event) {
+  if (event.target.type === "radio") {
+    deliveryOption = event.target.value;
+    updatePrice();
+  }
+}
 
-// const colorElements = document.querySelectorAll('.color');
-// const sizeElements = document.querySelectorAll('.size');
-
-// colorElements.forEach(colorElement => {
-//   colorElement.addEventListener('click', () => {
-//     const price = colorElement.getAttribute('data-price');
-//     document.getElementById('outprice').textContent = price;
-//   });
-// });
-
-// sizeElements.forEach(sizeElement => {
-//   sizeElement.addEventListener('click', () => {
-//     const price = sizeElement.getAttribute('data-price');
-//     document.getElementById('outprice').textContent = price;
-//   });
-// });
+function updatePrice() {
+  let newPrice = parseInt(activeColor.getAttribute("data-price"));
+  if (activeSize) {
+    newPrice += parseInt(activeSize);
+  }
+  if (deliveryOption === "express") {
+    newPrice += 10;
+  }
+  price.innerText = newPrice;
+}
--- JSF-topic-6/topic-6.js
+++ JSF-topic-6/topic-6.js
@@ -14,7 +14,9 @@ const lastEl = list.children[4].textContent;
 const secondEl = list.children[1].textContent;
 const fourthEl = list.children[3].textContent;
 const thirdEl = list.children[2].textContent;
-alert(firstEl + ', ' + lastEl + ', ' + secondEl + ', ' + fourthEl + ', ' + thirdEl);
+alert(
+  firstEl + ", " + lastEl + ", " + secondEl + ", " + fourthEl + ", " + thirdEl
+);
 
 //чи вивести по черзі кожне окремо ?
 // const list = document.getElementById("list");
@@ -24,24 +26,22 @@ alert(firstEl + ', ' + lastEl + ', ' + secondEl + ', ' + fourthEl + ', ' + third
 // alert(list.children[3].textContent);
 // alert(list.children[2].textContent);
 
-
 /*2. Напишіть скріпт, який за допомогою засобів DOM простилізує сторінку так як показано на картинці. */
 
-document.querySelector('h1').style.backgroundColor = '#90ee8f';
+document.querySelector("h1").style.backgroundColor = "#90ee8f";
 
-document.querySelectorAll('p').forEach(function (p, index) {
-  if (index === 0) p.style.fontWeight = 'bold';
-  if (index === 1) p.style.color = 'red';
-  if (index === 2) p.style.textDecoration = 'underline';
-  if (index === 3) p.style.fontStyle = 'italic';
+document.querySelectorAll("p").forEach(function (p, index) {
+  if (index === 0) p.style.fontWeight = "bold";
+  if (index === 1) p.style.color = "red";
+  if (index === 2) p.style.textDecoration = "underline";
+  if (index === 3) p.style.fontStyle = "italic";
 });
 
-document.querySelector('#myList').textContent;
-myList.style.display = 'flex';
-myList.style.listStyle = 'none';
+document.querySelector("#myList").textContent;
+myList.style.display = "flex";
+myList.style.listStyle = "none";
 
-document.querySelector('span').style.display = 'none';
-
+document.querySelector("span").style.display = "none";
 
 /* 3. Напишіть скріпт, який за допомогою засобів DOM створить для порожньої HTML-сторінки таку структуру 
 з тегів і їх атрибутів.
@@ -66,28 +66,25 @@ main.appendChild(div);
 body.appendChild(main);
 document.getElementsByTagName("html")[0].appendChild(body);
 
-
 /* 4. Реалізувати вивід даних із полів при кліку на кнопку "Надіслати" в поле outBlock */
 
-const submitBtn = document.querySelector('.btn');
+const submitBtn = document.querySelector(".btn");
 
-submitBtn.addEventListener('click', function (event) {
+submitBtn.addEventListener("click", function (event) {
   event.preventDefault();
   const fio = document.querySelector('input[name="fio"]').value;
   const phone = document.querySelector('input[name="phone"]').value;
   const birthday = document.querySelector('input[name="birthday"]').value;
   const email = document.querySelector('input[name="email"]').value;
 
-
   const resultStr = `ПІБ: ${fio}<br>
                       Номер телефону: ${phone}<br>
                       Дата народження: ${birthday}<br>
                       Електронна пошта: ${email}`;
 
-  const outBlock = document.querySelector('.out');
+  const outBlock = document.querySelector(".out");
   outBlock.innerHTML = resultStr;
 });
-
 
 /* 5. Задача описана в блоці JS
 1) вибрати всі теги із класом circle
@@ -100,13 +97,14 @@ submitBtn.addEventListener('click', function (event) {
 */
 const circles = document.querySelectorAll(".circle");
 
-circles.forEach(circle => {
+circles.forEach((circle) => {
   const animValue = circle.getAttribute("data-anim");
   circle.classList.add(animValue);
 });
 
-circles.forEach(circle => {
+circles.forEach((circle) => {
   console.log(circle.classList.contains(circle.getAttribute("data-anim")));
 });
 
 
+// задача 6 з кросівками лежить в окремій підпапці
