beb9fc1440893dcc647a57073cc28462a44108af
valeriasekret <leraguerra@gmail.com>
2023-04-10 13:48:34 +0200

    fixed topic 9

--- JSF-topic-9/topic-9.js
+++ JSF-topic-9/topic-9.js
@@ -3,12 +3,14 @@
 потім серед знайдених елементів знайдіть елементи з класом inner і поставте їм розмір 
 шрифту 35px.
 */
-$("h2.head").css("background-color", "green");
-$("h2.head span.inner").css("font-size", "35px");
+const $headings = $("h2.head");
+$headings.css("background-color", "green");
+$headings.find(".inner").css("font-size", "35px");
 
 
 /*9-2. На HTML-сторінці є посилання <a>. У випадку коли, href починається на https://, 
 потрібно додати посиланню атрибут target="_blank".*/
+
 $("a[href^='https://']").attr("target", "_blank");
 
 
@@ -25,9 +27,10 @@ $("h3").each(function () {
 
 /*9-4. На HTML-сторінці є 6 чекбоксів. Напишіть скріпт, який після того, як користувач
 позначив будь-які 3 чекбокси, всі чекбокси робить неактивними. */
-$(".myCheckbox").on("change", function () {
-    const ourCount = $(".myCheckbox:checked").length;
-    if (ourCount === 3) {
-        $(".myCheckbox").prop("disabled", true);
+const $checkboxes = $(".myCheckbox");
+$checkboxes.on("change", function () {
+    const ourCount = $checkboxes.filter(":checked").length;
+    if (ourCount >= 3) {
+        $checkboxes.prop("disabled", true);
     }
 });
