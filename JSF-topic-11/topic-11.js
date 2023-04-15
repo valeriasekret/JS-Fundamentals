cd8475e9c4967e8ff52fe2161a74dd2638fee2c1
valeriasekret <leraguerra@gmail.com>
2023-04-15 20:30:35 +0200

    topic 11 homework done

--- JSF-topic-11/topic-11.js
+++ JSF-topic-11/topic-11.js
@@ -1,1 +1,121 @@
+/*11-1.
+Реалізуйте функцію getPromise(message, delay), яка приймає текстове повідомлення message і 
+цілочисельне значення затримки delay (в мс) і повертає Promise, який чекає задану кількість 
+часу (використовуючи аргумент delay) і завершується повідомленням message.
+Приклад застосування функції:
+getPromise("test promise", 2000).then(function(data) {
+    console.log(data);        
+});
+Результат: через 2 сек в консолі виводиться "test promise" */
 
+function getPromise(message, delay) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve(message);
+        }, delay);
+    });
+}
+
+getPromise("test promise", 2000).then(function (data) {
+    console.log(data);
+});
+
+
+/*11-2.
+Реалізуйте функцію calcArrProduct(arr), яка приймає масив чисел. Функція повинна повернути 
+Promise, робота якого завершується поверненням добутку елементів масиву, якщо вони є типу 
+Numbers, або повідомленням "Error! Incorrect array!" у випадку, якщо хоча б 1 елемент масиву 
+нечисловий.
+Приклад застосування функції:
+calcArrProduct([3,4,5]).then(result => console.log(result)); // 60
+calcArrProduct([5,"user2", 7, 12]).then(result => console.log(result));
+// "Error! Incorrect array!"
+*/
+
+function calcArrProduct(arr) {
+    return new Promise(function (resolve, reject) {
+        let product = 1;
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === 'number') {
+                product *= arr[i];
+            } else {
+                reject('Error! Incorrect array!');
+                return;
+            }
+        }
+        resolve(product);
+    });
+}
+
+// calcArrProduct([3,4,5]).then(result => console.log(result)); // 60
+// calcArrProduct([5,"user2", 7, 12]).then(result => console.log(result));
+// // "Error! Incorrect array!"
+
+
+/* 11-3.Створіть наступний асинхронний ланцюжок promise:
+new Promise(function (resolve, reject) {
+   // Запитуємо у користувача number за допомогою prompt()
+   // Якщо користувач ввів не число - викликаємо reject()
+   // Якщо користувач ввів число- викликаємо resolve(number)
+}).catch(function (error) {
+   return new Promise(function (resolve, reject) {
+      // Запитуємо у користувача number, до тих пір, поки він його не введе
+      // Після вводу числа - викликаємо resolve(number)
+   });
+}).then(function (result) {
+   // Вивід number у консоль
+});*/
+
+function getUserNumber() {
+    return new Promise(function(resolve, reject) {
+      let number = prompt("Please enter a number:");
+      if (isNaN(number)) {
+        reject(new Error("Input is not a number"));
+      } else {
+        resolve(Number(number));
+      }
+    });
+  }
+  
+  getUserNumber()
+    .catch(function(error) {
+      console.error(error);
+      return getUserNumber();
+    })
+    .then(function(result) {
+      console.log("Number entered:", result);
+    });
+
+    
+/* 11-4.
+Заданий цикл for, який виводить послідовність чисел від 0 до 10 з випадковим інтервалом 
+(від 0 до N мілісекунд). Використовуючи проміси потрібно змінити цикл так, щоб числа виводилися 
+в строгій послідовності від 0 до 10. Наприклад, якщо виведення нуля займає 4 секунди, а одиниці
+ 2 секунди, то одиниця повинна дочекатися виведення  нуля і тільки потім почати свій відлік 
+ (щоб дотримуватися послідовності).
+Для розв’язку задачі необзідно застосувати задану функцію delay(i, time), яка повертає проміс, 
+який резолвиться поточним значенням числа-лічильника циклу і, яке виводиться через час time 
+мілісекунд.
+Приклад  роботи:
+const delay = (i, time) => new Promise(resolve => setTimeout(() => resolve(i), time));
+function showNumbers() {
+        // your code with using delay(i, time)
+}
+showNumbers();
+
+Результат: 
+з різним рендомним інтервалом (наприклад від 0 до 3 сек) в консоль послідовно  виводяться 
+числа 0,1,2,3,4,5,6,7,8,9 Задачу потрібно реалізувати без застосування промісів. Переписати 
+функцію showNumbers, використовуючи конструкцію async/await.
+*/
+
+const delay = (i, time) => new Promise(resolve => setTimeout(() => resolve(i), time));
+
+async function showNumbers() {
+  for (let i = 0; i < 10; i++) {
+    await delay(i, Math.floor(Math.random() * 3000));
+    console.log(i);
+  }
+}
+
+showNumbers();
--- JSF-topic-12/topic-12.js
+++ JSF-topic-12/topic-12.js
@@ -1,1 +1,169 @@
+/*12-1.Створити Node.js http-сервер, який слухатиме запити на порт 5000 на локальній  машині. 
+Сервер повинен повертати сторінку, що містить ім’я поточного користувача операційної системи, 
+тип операційної системи, час роботи системи в хвилинах (використати модуль OS), поточну робочу
+ директорію і назву файлу сервера (використати модуль path). */
 
+const http = require('http');
+const os = require('os');
+const path = require('path');
+
+function generateResponse() {
+    const userInfo = os.userInfo();
+    const uptimeInMinutes = Math.floor(os.uptime() / 60);
+    const serverFileName = path.basename(__filename);
+
+    return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Server Info</title>
+      </head>
+      <body>
+        <h1>Server Info</h1>
+        <p><strong>User:</strong> ${userInfo.username}</p>
+        <p><strong>OS Type:</strong> ${os.type()}</p>
+        <p><strong>Uptime (minutes):</strong> ${uptimeInMinutes}</p>
+        <p><strong>Current Directory:</strong> ${process.cwd()}</p>
+        <p><strong>Server File Name:</strong> ${serverFileName}</p>
+      </body>
+    </html>
+  `;
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(generateResponse());
+});
+
+server.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
+});
+
+//http://localhost:5000
+
+/*12-2. Необхідно створити власний модуль personalmodule.js, який містить функцію, що приймає 
+ім’я системного юзера і працює з поточним часом та на основі пори доби виводить повідомлення 
+із привітанням юзера. Щоб експортувати змінні чи функції модуля на зовні можна використати 
+об’єкт module.exports. 
+        Створіть Node.js сервер, який з використанням функціоналу розробленого модуля
+         повертатиме наступну сторінку:
+ */
+
+function getGreeting(username) {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+    let greeting = '';
+
+    if (currentHour < 12) {
+        greeting = `Доброго ранку, ${username}!`;
+    } else if (currentHour < 18) {
+        greeting = `Доброго дня, ${username}!`;
+    } else {
+        greeting = `Доброго вечора, ${username}!`;
+    }
+
+    return greeting;
+}
+
+module.exports = {
+    getGreeting
+};
+
+const http = require('http');
+const os = require('os');
+const personalModule = require('./personalmodule'); // Імпортуємо розроблений модуль
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    const username = os.userInfo().username;
+
+    const greeting = personalModule.getGreeting(username);
+
+    res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Привітання</title>
+      </head>
+      <body>
+        <h1>Привітання</h1>
+        <p>${greeting}</p>
+      </body>
+    </html>
+  `);
+});
+
+server.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
+});
+
+
+/*12-3 ⭐⭐⭐
+Створіть просту програму на Node.js, яка записує у файл текстову інформацію, яку 
+користувач вводить з адресного рядка, а потім зчитує цей файл та виводить вміст на екран.
+
+Кроки:
+1. Створити змінну, яка зберігатиме шлях до файлу, у який буде записана інформація. 
+2. Використовуючи модуль fs (file system), створити функцію, яка дозволить записувати 
+інформацію в файл. 
+3. Використовуючи модуль http, створити сервер, який буде прослуховувати запити з адресного 
+рядка та передавати отриману інформацію функції writeToTextFile(). 
+4. Для зчитування даних з файлу, використовуйте модуль fs та функцію readFile()
+5. Для того, щоб вивести зчитану інформацію на екран, додайте відповідну логіку до серверу. 
+ */
+
+// Імпортуємо модулі
+const http = require('http');
+const fs = require('fs');
+
+// Встановлюємо порт, на якому буде працювати сервер
+const PORT = 3000;
+
+// Шлях до файлу, у який буде записуватись інформація
+const filePath = 'data.txt';
+
+// Створюємо сервер
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let data = '';
+
+    // Обробка події отримання даних від користувача
+    req.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // Обробка події завершення передачі даних від користувача
+    req.on('end', () => {
+      // Записуємо отриману інформацію у файл
+      fs.appendFile(filePath, data, (err) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Помилка сервера');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end('Дані успішно записано у файл');
+        }
+      });
+    });
+  } else if (req.method === 'GET') {
+    // Зчитуємо дані з файлу
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Помилка сервера');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 - Сторінка не знайдена');
+  }
+});
+
+// Запускаємо сервер на вказаному порті
+server.listen(PORT, () => {
+  console.log(`Сервер запущено на порту ${PORT}`);
+});
